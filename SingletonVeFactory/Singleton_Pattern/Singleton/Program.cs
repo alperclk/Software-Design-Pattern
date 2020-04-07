9d46e6489b35dfa1a4df810d3d1959aed5ce7306
alperclk <160202023@kocaeli.edu.tr>
2020-04-07 22:16:29 +0300

    Add files via upload

---
+++ SingletonVeFactory/Singleton_Pattern/Singleton/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SingletonNesne a = SingletonNesne.NesneOlustur();
+            SingletonNesne b = SingletonNesne.NesneOlustur();
+
+            if (a == b)
+            {
+                Console.WriteLine("Olusturulan a ve b nesneleri eşittir");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ SingletonVeFactory/Singleton_Pattern/Singleton/SingletonNesne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class SingletonNesne
+    {
+        private static SingletonNesne dondurulecek;
+        private SingletonNesne()
+        {
+
+        }
+        public static SingletonNesne NesneOlustur()
+        {
+            if (dondurulecek == null)
+            {
+                dondurulecek = new SingletonNesne();
+            }
+            return dondurulecek;
+        }
+
+    }
+}
