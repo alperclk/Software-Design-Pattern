76bda0602e294a4682c3a57e1b2b009fe8a15590
alperclk <160202023@kocaeli.edu.tr>
2020-04-07 22:08:48 +0300

    Add files via upload

---
+++ SingletonVeFactory/Factory_Pattern/FactoryPattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factory factory = new Factory();
+
+            Sekil ucgen = factory.SekilDondur("Ucgen");
+            ucgen.CevreHesapla(4);
+
+            Sekil kare = factory.SekilDondur("Kare");
+            kare.CevreHesapla(4);
+
+            Sekil altigen = factory.SekilDondur("Altigen");
+            altigen.CevreHesapla(4);
+
+            Console.Read();
+        }
+    }
+}
---
+++ SingletonVeFactory/Factory_Pattern/FactoryPattern/Sekil.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public interface Sekil
+    {
+        void CevreHesapla(int kenar);
+    }
+
+    public class Ucgen : Sekil
+    {
+        public void CevreHesapla(int kenar)
+        {
+            Console.WriteLine("Eşkenar üçgenin çevresi {0}", kenar * 3);
+        }
+    }
+
+    public class Kare : Sekil
+    {
+        public void CevreHesapla(int kenar)
+        {
+            Console.WriteLine("Karenin çevresi {0}", kenar * 4);
+        }
+    }
+
+    public class Altigen : Sekil
+    {
+        public void CevreHesapla(int kenar)
+        {
+            Console.WriteLine("Altigenin çevresi {0}", kenar * 6);
+        }
+    }
+
+}
