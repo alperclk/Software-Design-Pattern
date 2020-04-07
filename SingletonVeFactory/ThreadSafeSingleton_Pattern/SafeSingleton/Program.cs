654fbcd258ce72123d680b57bb38ae0a005cfa3b
alperclk <160202023@kocaeli.edu.tr>
2020-04-07 22:12:06 +0300

    Add files via upload

---
+++ SingletonVeFactory/ThreadSafeSingleton_Pattern/SafeSingleton/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(() => BirinciKosucu(), () => IkinciKosucu());     
+            
+        }
+
+        private static void BirinciKosucu()
+        {
+            SingletonKupa birinciKosucu = SingletonKupa.KupayiAl;
+            Console.WriteLine("Birinci kosucu");
+        }
+        private static void IkinciKosucu()
+        {
+            SingletonKupa ikinciKosucu = SingletonKupa.KupayiAl;
+            Console.WriteLine("İkinci kosucu");
+            Console.ReadKey();
+            
+        }
+
+    }
+}
---
+++ SingletonVeFactory/ThreadSafeSingleton_Pattern/SafeSingleton/SingletonKupa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeSingleton
+{
+    public sealed class SingletonKupa
+    {
+        private static int counter = 0;
+        private SingletonKupa()
+        {
+            Console.WriteLine("{0} kere constructor methodu calisti", counter);
+        }
+        private static SingletonKupa singletonKupa = null;
+        private static readonly object Lock = new object();
+        
+        public static SingletonKupa KupayiAl
+        {
+            get
+            {
+                lock (Lock)
+                {
+                    if (singletonKupa == null)
+                    {
+                        counter++;
+                        singletonKupa = new SingletonKupa();
+                        
+                    }
+                }
+                return singletonKupa;
+            }
+        }
+
+    }
+}
