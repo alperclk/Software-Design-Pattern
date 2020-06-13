a4792c4e69922e5e18f2afd132abce85187efc2d
alperclk <160202023@kocaeli.edu.tr>
2020-06-13 23:46:26 +0300

    Add files via upload

---
+++ LiskovSubstitutionPrinciple/Liskov/Liskov/Genc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liskov
+{
+    class Genc : Insan, IDisariCikabilir
+    {
+        public Genc(string AdSoyad,int Yas)
+        {
+            this.AdSoyad = AdSoyad;
+            this.Yas = Yas;
+        }
+        public void DisariCik()
+        {
+            Console.WriteLine("Genc {0} kişisi ({1}yaşında) dışarı çıkabilir", this.AdSoyad, this.Yas);
+        }
+    }
+}
---
+++ LiskovSubstitutionPrinciple/Liskov/Liskov/Insan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liskov
+{
+    public abstract class Insan
+    {
+        public void BilgiYazdir()
+        {
+            Console.WriteLine("AdSoyad = {0} , Yaş = {1}", this.AdSoyad, this.Yas);
+        }
+        public int Yas { get; set; }
+        public string AdSoyad { get; set; }
+        public void TvIzle()
+        {
+            Console.WriteLine("Insan {0}, kişisi tv izliyor.", this.AdSoyad);
+        }
+        public void YemekYe()
+        {
+            Console.WriteLine("Insan {0}, kişisi tv izliyor.", this.AdSoyad);
+        }
+    }
+}
