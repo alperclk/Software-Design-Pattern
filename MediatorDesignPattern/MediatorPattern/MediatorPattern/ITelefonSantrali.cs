11e3b3252efa40f50e73228d403368a5bcb4f610
alperclk <160202023@kocaeli.edu.tr>
2020-06-13 23:48:50 +0300

    Add files via upload

---
+++ MediatorDesignPattern/MediatorPattern/MediatorPattern/ITelefonSantrali.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorPattern
+{
+    interface ITelefonSantrali //Mediator Interface
+    {
+        void MesajGonder(string mesaj, Kullanici kullanici, string gonderilenNo);
+        void KullaniciEkle(Kullanici kullanici);
+    }
+}
---
+++ MediatorDesignPattern/MediatorPattern/MediatorPattern/Kullanici.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorPattern
+{
+    abstract class Kullanici
+    {
+        protected ITelefonSantrali telefonSantrali { get; set; }
+        public string kullaniciAdi { get; set; }
+        public string kullaniciNo { get; set; }
+
+        public Kullanici(ITelefonSantrali santral,string kAdi,string kNo)
+        {
+            this.telefonSantrali = santral;
+            this.kullaniciAdi = kAdi;
+            this.kullaniciNo = kNo;
+        }
+        public abstract void MesajAl(string mesaj, Kullanici kullanici);
+        public abstract void MesajGonder(string mesaj, string gonderilenNo);
+    }
+}
